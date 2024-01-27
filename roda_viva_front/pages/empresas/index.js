ef90b05cd6ccdc889ab1eb64b8f0b67170fe1e87
Vitor Gabriel <sgvitor@outlook.com.br>
2024-01-27 11:23:36 -0300

    empresas get, post

--- roda_viva_front/pages/empresas/index.js
+++ roda_viva_front/pages/empresas/index.js
@@ -1,126 +1,46 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function index() {
+function empresas() {
+    const [empresas, setEmpresas] = useState([]);
+    useEffect(() => {
+        axios
+            .get("https://localhost:7226/api/empresas")
+            .then((response) => {
+                setEmpresas(response.data);
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar lista de cadastros", error);
+            });
+    }, []);
     return (
         <>
             {/*Corpo com as empresas parceiras*/}
             <main className="container my-3" id="listaParceiros">
-                <section className="row" style={{ marginTop: 100 }}>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/BorceleArquitetura.png"
-                            alt=""
-                        />
-                        <p className="text-center">Borcele Arquitetura</p>
-                    </div>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/DuarteCorretora.png"
-                            alt=""
-                        />
-                        <p className="text-center">Estela Duarte Corretora</p>
-                        <p></p>
-                    </div>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/DjVitor.png"
-                            alt=""
-                        />
-                        <p className="text-center">DJ Vitor Produções</p>
-                        <p></p>
-                    </div>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/FaustinoAndaimes.png"
-                            alt=""
-                        />
-                        <p className="text-center">Faustino Andaimes</p>
-                        <p></p>
-                    </div>
-                </section>
-                <section className="row">
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/JoseMestreObra.png"
-                            alt=""
-                        />
-                        <p className="text-center">José Mestre de Obras</p>
-                        <p></p>
-                    </div>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/MariaDoceria.png"
-                            alt=""
-                        />
-                        <p className="text-center">Maria Doceria</p>
-                        <p></p>
-                    </div>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/PenseSa.png"
-                            alt=""
-                        />
-                        <p className="text-center">Pense S.A.</p>
-                        <p></p>
-                    </div>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/ShopOnline.png"
-                            alt=""
-                        />
-                        <p className="text-center">Shopping Online</p>
-                        <p></p>
-                    </div>
-                </section>
-                <section className="row">
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/ThiagoPastelaria.png"
-                            alt=""
-                        />
-                        <p className="text-center">Thiago Pastelaria</p>
-                        <p></p>
-                    </div>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/WaschingtonBarber.png"
-                            alt=""
-                        />
-                        <p className="text-center">Waschington Barbearia</p>
-                        <p></p>
-                    </div>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/WelintonBar.png"
-                            alt=""
-                        />
-                        <p className="text-center">Welinton Bar</p>
-                        <p></p>
-                    </div>
-                    <div className="col-sm border-bottom ">
-                        <img
-                            className="img-fluid"
-                            src="img/img-parceiros/YuriPadaria.png"
-                            alt=""
-                        />
-                        <p className="text-center">Yuri Padaria</p>
-                        <p></p>
-                    </div>
-                </section>
+                {empresas.map((empresa) => (
+                    <section className="row" style={{ marginTop: 100 }}>
+                        <div
+                            className="col-sm border-bottom "
+                            key={empresa.idEmpresa}
+                        >
+                            {/*<img
+                                className="img-fluid"
+                                src="img/img-parceiros/BorceleArquitetura.png"
+                                alt=""
+                />*/}
+                            <h2 className="text-center">{empresa.nome}</h2>
+                            <p>{}</p>
+                            <p>{empresa.email}</p>
+                            <p>{empresa.telefone}</p>
+                            <p>{empresa.cnpj}</p>
+                            <p>{empresa.endereço}</p>
+                            <p>{empresa.cep}</p>
+                        </div>
+                    </section>
+                ))}
             </main>
         </>
     );
 }
 
-export default index;
+export default empresas;
